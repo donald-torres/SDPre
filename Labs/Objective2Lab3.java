c96add79aac4eb6107e86e29f666d07d2e1ab6dd
Donald Torres <dctorres5597@gmail.com>
2023-09-12 16:57:31 -0600

    finish Objective4Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int Year = 1986;
+    String Month = "March";
+    Boolean Weekend = false;
+
+    System.out.println(Year);
+    System.out.println(Month);
+    System.out.println(Weekend);
+  }
+}
